849b5fd34c5ddacb056651959fb877d66be79198
Vita Kesso <buvitabu@gmail.com>
2025-08-24 18:50:32 +0300

    feat: enemy random

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
 document.getElementById('fight').addEventListener('click', function() {
    document.querySelector('.first').classList.remove('active')
    document.querySelector('.main').innerHTML = 'Battle'
-   console.log(fight)
 })
